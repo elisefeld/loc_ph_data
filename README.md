fde672c047c851b600883c16eff79eea668504cd
esfeld <eshachfeld@icloud.com>
2024-12-13 23:20:54 -0600

    adding code comments

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # loc_ph_data
+
+
+Data 
+----------------
+The data used for the analysis is stored in the Data folder. 
+All data files were filtered to include English results only. 
+pearl_data.rds contains data for dates between September 7th, 1941 and March 7th, 1941. 
+
+----------------
+data_prep.R and text_analysis.R contain functions used to prepare and analyze the data.
--- data_prep.R
+++ data_prep.R
@@ -7,16 +7,45 @@ library(jsonlite)
 base_url <- "https://www.loc.gov/collections/chronicling-america/"
 final_results <- data.frame()
 
+iso_to_language <- data.frame( 
+  other_language = c("english",
+                     "spanish",
+                     "finnish",
+                     "yiddish",
+                     "german",
+                     "czech",
+                     "croatian",
+                     "polish",
+                     "italian",
+                     "slovenian",
+                     "hungarian",
+                     "romanian",
+                     "japanese"),
+  iso_code = c("en",
+               "es",
+               "fi",
+               "yi",
+               "de",
+               "cs",
+               "hr",
+               "pl",
+               "it",
+               "sl",
+               "hu",
+               "ro",
+               "ja")
+)
 
-get_json_data <- function(params) {
-  req <- request(base_url) |>
-    req_throttle(rate = 20/10) |>
-    req_url_query(!!!params)
+
+get_json_data <- function(params) { 
+  req <- request(base_url) |> # 
+    req_throttle(rate = 20/10) |> # do not exceed LOC rate limit 
+    req_url_query(!!!params) # add query parameters
   
   response <- req |>
-    req_perform() |>
-    resp_body_string() |>
-    fromJSON()
+    req_perform() |> 
+    resp_body_string() |> # return utf-8 string
+    fromJSON() # convert from JSON
   
   return(response)
 }
@@ -25,10 +54,10 @@ get_json_data <- function(params) {
 
 parse_json_data <- function(response) {
   data <- flatten(response$results)
-  has_more <- !is.null(response$pagination["next"][[1]])
-  print(paste0(response$pagination$to, "/", response$pagination$of))
-  print(paste0("Has more pages: ", has_more))
-  mylist <- list(data, has_more)
+  has_more <- !is.null(response$pagination["next"][[1]]) # check if there are more pages
+  print(paste0(response$pagination$to, "/", response$pagination$of)) # print the status of the request
+  print(paste0("Has more pages: ", has_more)) # print if there are more pages
+  mylist <- list(data, has_more) 
   return(mylist)
 }
 
@@ -46,7 +75,7 @@ get_paginated_data <- function(display_level = "page",
   page <- 1
   has_more <- TRUE
   
-  while (has_more) {
+  while (has_more) { # loop over all pages until there are none left
     print(paste0("Getting page ", page, "..."))
     
     # Set the query parameters
@@ -69,14 +98,15 @@ get_paginated_data <- function(display_level = "page",
     
     final_results <- bind_rows(final_results, parsed)
     
-    page <- page + 1
+    page <- page + 1 # proceed to next page
   }
-  print(paste0("Number of results: ", nrow(final_results)))
-  final_results <<- final_results
+  print(paste0("Number of results: ", nrow(final_results))) 
+  final_results <<- final_results 
 }
 
 
-
+# I recognize this function appears redundant considering you can input a range directly,
+# however when requesting a large range at once we experienced issues with the API
 get_all_years <- function(start_year = "", end_year = "") {
   
   all_data <- list()
@@ -93,7 +123,8 @@ get_all_years <- function(start_year = "", end_year = "") {
   }
   raw_data <- bind_rows(data_list)
   
-  saveRDS(raw_data, "year_data.rds")
+  saveRDS(raw_data, "year_data.rds") # save the data to an r file 
+  # (can't be saved to a csv because of the nested structure)
   
   return(raw_data)
 }
@@ -118,55 +149,26 @@ prepare_data <- function(data = "raw_data.rds") {
            publication_frequency,
            subject,
            subject_ethnicity) |>
-    mutate(across(where(is.list) & !all_of("subject"),
+    mutate(across(where(is.list) & !all_of("subject"), # remove list columns
                   ~ sapply(.x, function(val) paste(val, collapse = ', ')))) |>
-    mutate(language = str_remove(language, "[, ]?english[, ]?"),
-           language = str_remove(language, ","),
+    mutate(language = str_remove(language, "[, ]?english[, ]?"), # remove english from the language column
+           language = str_remove(language, ","), 
            language = str_trim(language)) |>
     rename(newspaper_title = partof_title,
            text_data = description,
            state = location_state,
            city = location_city,
            other_language = language) |>
-    mutate(across(everything(), ~ ifelse(. == "", NA, .))) |>
-    mutate(across(everything(), ~ ifelse(. == "NULL", NA, .))) |>
-    mutate(as_date = ymd(date)) |> 
-    filter(as_date >= "1941-09-07" & as_date <= "1942-03-07") |>
-    mutate(before_pearl_harbor = ifelse(as_date < "1941-12-07", "Before Pearl Harbor", "After Pearl Harbor"),
-          newspaper_name = str_remove(newspaper_title, " \\(.*"),
-          newspaper_name = str_trim(newspaper_name))
-  
-  iso_to_language <- data.frame( 
-    other_language = c("english",
-                       "spanish",
-                       "finnish",
-                       "yiddish",
-                       "german",
-                       "czech",
-                       "croatian",
-                       "polish",
-                       "italian",
-                       "slovenian",
-                       "hungarian",
-                       "romanian",
-                       "japanese"),
-    iso_code = c("en",
-                 "es",
-                 "fi",
-                 "yi",
-                 "de",
-                 "cs",
-                 "hr",
-                 "pl",
-                 "it",
-                 "sl",
-                 "hu",
-                 "ro",
-                 "ja")
-  )
+    mutate(across(everything(), ~ ifelse(. == "", NA, .))) |> # replace empty strings with NA
+    mutate(across(everything(), ~ ifelse(. == "NULL", NA, .))) |> # replace NULL with NA
+    mutate(as_date = ymd(date)) |> # convert date to date format
+    filter(as_date >= "1941-09-07" & as_date <= "1942-03-07") |> # filter by date range
+    mutate(before_pearl_harbor = ifelse(as_date < "1941-12-07", "Before Pearl Harbor", "After Pearl Harbor"), # create a new column for before or after Pearl Harbor
+          newspaper_name = str_remove(newspaper_title, " \\(.*"), # remove anything after parentheses from the newspaper title
+          newspaper_name = str_trim(newspaper_name)) 
   
   pearl_data <- pearl_data |>
-    left_join(iso_to_language, by = "other_language")
+    left_join(iso_to_language, by = "other_language") # join in the iso codes
   
   return(pearl_data)
 }
